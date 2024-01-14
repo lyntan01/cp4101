30e5ba2a86e3d353fb679dd66f1906df418d8156
Lyn Tan <lyntan2001@outlook.com>
2024-01-14 16:39:49 +0800

    Add DAO Classes

---
+++ backend/src/dao/CourseDao.ts
@@ -0,0 +1,45 @@
+import { Course, Prisma, PrismaClient } from "@prisma/client";
+
+export class CourseDao {
+  constructor(private prismaClient: PrismaClient = new PrismaClient()) {}
+
+  public async createCourse(
+    courseData: Prisma.CourseCreateInput
+  ): Promise<Course> {
+    return this.prismaClient.course.create({
+      data: courseData,
+    });
+  }
+
+  public async getAllCourses(): Promise<Course[]> {
+    return this.prismaClient.course.findMany();
+  }
+
+  public async getCourseById(courseId: string): Promise<Course | null> {
+    return this.prismaClient.course.findUnique({
+      where: { id: courseId },
+      include: {
+        teacher: true,
+        students: true,
+        chapters: true,
+        analytics: true,
+      },
+    });
+  }
+
+  public async updateCourse(
+    courseId: string,
+    courseData: Prisma.CourseUpdateInput
+  ): Promise<Course | null> {
+    return this.prismaClient.course.update({
+      where: { id: courseId },
+      data: courseData,
+    });
+  }
+
+  public async deleteCourse(courseId: string): Promise<Course | null> {
+    return this.prismaClient.course.delete({
+      where: { id: courseId },
+    });
+  }
+}
---
+++ backend/src/dao/PageDao.ts
@@ -0,0 +1,43 @@
+import { Page, Prisma, PrismaClient } from "@prisma/client";
+
+export class PageDao {
+  constructor(private prismaClient: PrismaClient = new PrismaClient()) {}
+
+  public async createPage(pageData: Prisma.PageCreateInput): Promise<Page> {
+    return this.prismaClient.page.create({
+      data: pageData,
+    });
+  }
+
+  public async getAllPages(): Promise<Page[]> {
+    return this.prismaClient.page.findMany();
+  }
+
+  public async getPageById(pageId: string): Promise<Page | null> {
+    return this.prismaClient.page.findUnique({
+      where: { id: pageId },
+      include: {
+        traditionalTextBasedLessonPage: true,
+        codeSandboxPage: true,
+        stepByStepVisualizationPage: true,
+        realTimeCodeFeedbackPage: true,
+      },
+    });
+  }
+
+  public async updatePage(
+    pageId: string,
+    pageData: Prisma.PageUpdateInput
+  ): Promise<Page | null> {
+    return this.prismaClient.page.update({
+      where: { id: pageId },
+      data: pageData,
+    });
+  }
+
+  public async deletePage(pageId: string): Promise<Page | null> {
+    return this.prismaClient.page.delete({
+      where: { id: pageId },
+    });
+  }
+}
