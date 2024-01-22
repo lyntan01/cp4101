ec8915ae7544e5b2b3b0224bda023fd98331b8df
Lyn Tan <lyntan2001@outlook.com>
2024-01-23 00:01:31 +0800

    Format create text page

--- frontend/src/pages/lesson-pages/CreateTraditionalTextPage.tsx
+++ frontend/src/pages/lesson-pages/CreateTraditionalTextPage.tsx
@@ -5,13 +5,14 @@ import { CreateTextPageData, createTextPage } from "../../api/textPage";
 import { LexEditor } from "../../rich-text-editor";
 import { Chapter } from "../../types/models";
 import { useToast } from "../../wrappers/ToastProvider";
+import { GenericButton } from "../../components/buttons";
 
 const CreateTraditionalTextPage: React.FC = () => {
   const { chapterId } = useParams();
   const [chapter, setChapter] = useState<Chapter>();
   const [traditionalTextPageData, setTraditionalTextPageData] =
     useState<CreateTextPageData>({
-      title: "Sample Title 2",
+      title: "",
       content: "",
       chapterId: chapterId ?? "",
     });
@@ -38,6 +39,10 @@ const CreateTraditionalTextPage: React.FC = () => {
 
   const handleSubmit = async () => {
     try {
+      if (traditionalTextPageData.title.trim().length === 0) {
+        displayToast("Page title cannot be empty!", ToastType.ERROR);
+        return;
+      }
       const newPageResponse = await createTextPage(traditionalTextPageData);
       displayToast("Text lesson page created successfully.", ToastType.INFO);
       navigate(`/pages/${newPageResponse.data.pageId}`);
@@ -59,27 +64,90 @@ const CreateTraditionalTextPage: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <div className="flex justify-between items-center">
-        <span className="text-2xl font-bold tracking-wide text-gray-800">
-          Create new text lesson page
-        </span>
-        <span className="font-light tracking-wide text-gray-600">
-          in {chapter?.name}
-        </span>
+    <div className="pb-12">
+      <h2 className="text-2xl font-bold tracking-wide text-gray-800">
+        Create new text lesson page
+      </h2>
+      <p className="mt-1 text-sm leading-6 font-light tracking-wide text-gray-600">
+        in {chapter?.name}
+      </p>
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Page Title
+          </label>
+          <div className="mt-2">
+            <input
+              id="title"
+              name="title"
+              type="text"
+              value={traditionalTextPageData.title}
+              onChange={(e) =>
+                setTraditionalTextPageData({
+                  ...traditionalTextPageData,
+                  title: e.target.value,
+                })
+              }
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div className="sm:col-span-6">
+          <label
+            htmlFor="content"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Content
+          </label>
+          <div className="mt-2">
+            <LexEditor
+              onChange={(value) =>
+                setTraditionalTextPageData({
+                  ...traditionalTextPageData,
+                  content: value,
+                })
+              }
+              editorStateStr={traditionalTextPageData.content}
+            />
+          </div>
+        </div>
       </div>
-      <LexEditor
-        onChange={(value) =>
-          setTraditionalTextPageData({
-            ...traditionalTextPageData,
-            content: value,
-          })
-        }
-        editorStateStr={traditionalTextPageData.content}
-      />
-      <button onClick={() => handleSubmit()}>Create</button>
+      <div className="px-4 py-8 rounded space-y-4 flex flex-col">
+        <GenericButton
+          type="submit"
+          text="Create Page"
+          onClick={handleSubmit}
+          className="self-center bg-sky-600 hover:bg-sky-700"
+        />
+      </div>
     </div>
   );
 };
 
 export default CreateTraditionalTextPage;
+
+<div className="border-b border-gray-900/10 pb-12">
+  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    <div className="sm:col-span-4">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Email address
+      </label>
+      <div className="mt-2">
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+  </div>
+</div>;
